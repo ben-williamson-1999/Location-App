fc10a31df79ff3fa5be7e76b6dfacbbc18376bcf
ben-williamson-1999 <ben.williamson1@ntlworld.com>
2019-02-12 13:00:50 +0000

    Initial commit

---
+++ app/src/main/java/com/example/locationapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.locationapp;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private double lat;
+
+    private double lng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) throws SecurityException {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{
+                "android.permission.ACCESS_FINE_LOCATION",
+                "android.permission.ACCESS_COARSE_LOCATION"}, 1);
+
+        } else {
+            LocationManager ls = (LocationManager) getSystemService(LOCATION_SERVICE);
+            ls.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    lat = location.getLatitude();
+                    lng = location.getLongitude();
+                    ((TextView) findViewById(R.id.latTextView)).setText("" + lat);
+                    ((TextView) findViewById(R.id.lngTextView)).setText("" + lng);
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+                @Override
+                public void onProviderEnabled(String provider) { }
+
+                @Override
+                public void onProviderDisabled(String provider) { }
+            });
+        }
+    }
+}
+
